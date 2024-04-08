2616f02cf13009fbd4253f24a4faef8fc49d3bf5
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-08 01:32:08 -0300

    feat: add signin navigation

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -1,15 +1,15 @@
 import { Button, IconButton } from "@chakra-ui/react";
 import { FiLogIn } from "react-icons/fi";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 
 export default function Header() {
   return (
     <header className="flex justify-center items-center w-full bg-white drop-shadow p-2">
       <h1 className="absolute text-3xl text-center">Todo</h1>
-      <IconButton
-        className="ml-auto"
-        aria-label="Login"
-        icon={<FiLogIn fontSize={20} />}
-      />
+      <Link className="ml-auto" as={NextLink} href="/signin">
+        <IconButton aria-label="Login" icon={<FiLogIn fontSize={20} />} />
+      </Link>
     </header>
   );
 }
