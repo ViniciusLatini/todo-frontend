a907287109956894c2275e8d882fb52f987fb8d5
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-07 23:25:32 -0300

    feat: create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+import { Button, SimpleGrid } from "@chakra-ui/react";
+import { FiPlus } from "react-icons/fi";
+import Header from "./components/global/Header";
+import Options from "./components/home/Options";
+import TodoCard from "./components/home/TodoCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>fmdaslfka</h1>
-    </main>
+    <>
+      <Header />
+      <main className="py-5 px-10 ">
+        <Options />
+
+        <SimpleGrid marginTop="20px" minChildWidth="320px" spacing="40px">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </SimpleGrid>
+      </main>
+    </>
   );
 }
