e29b6d2bc6309de8fb6b9cbac27cd77f73e9b67d
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-18 18:44:38 -0300

    feat: getAll todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import { Button, SimpleGrid } from "@chakra-ui/react";
-import { FiPlus } from "react-icons/fi";
-import Header from "./components/global/Header";
-import Options from "./components/home/Options";
-import TodoCard from "./components/home/TodoCard";
+import { SimpleGrid } from "@chakra-ui/react";
+import Header from "@/components/global/Header";
+import Options from "@/components/home/Options";
+import TodoCard from "@/components/home/TodoCard";
+import { api } from "@/data/axios";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
+  async function getUserTodos() {
+    const { data } = await api.get("/todo/6611b32bd999e9040c7c95f3");
+    return data.todos as Array<any>;
+  }
+
+  const todos = await getUserTodos();
+  console.log(todos, "tododo");
   return (
     <>
       <Header />
@@ -12,12 +20,13 @@ export default function Home() {
         <Options />
 
         <SimpleGrid marginTop="20px" minChildWidth="320px" spacing="40px">
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
-          <TodoCard />
+          <Suspense fallback={<h1>loading</h1>}>
+            {todos.map((todo) => (
+              <>
+                <TodoCard key={todo.id} />
+              </>
+            ))}
+          </Suspense>
         </SimpleGrid>
       </main>
     </>
