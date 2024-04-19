d19a726d15428002733a101e4a60e94da6cf19e1
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2024-04-19 00:29:47 -0300

    feat: get all users todos with cookies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
-import { SimpleGrid } from "@chakra-ui/react";
 import Header from "@/components/global/Header";
 import Options from "@/components/home/Options";
-import TodoCard from "@/components/home/TodoCard";
+import { cookies } from "next/headers";
 import { api } from "@/data/axios";
-import { Suspense } from "react";
+import { Todo } from "@/data/types/todo";
+import TodosContainer from "@/components/home/TodosContainer";
+
+async function getUserTodos() {
+  try {
+    const id = cookies().get("id")?.value;
+    const { data } = await api.get(`/todo/${id}`);
+    return data.todos as Todo[];
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default async function Home() {
-  async function getUserTodos() {
-    const { data } = await api.get("/todo/6611b32bd999e9040c7c95f3");
-    return data.todos as Array<any>;
-  }
-
   const todos = await getUserTodos();
-  console.log(todos, "tododo");
   return (
     <>
       <Header />
       <main className="py-5 px-10 ">
         <Options />
-
-        <SimpleGrid marginTop="20px" minChildWidth="320px" spacing="40px">
-          <Suspense fallback={<h1>loading</h1>}>
-            {todos.map((todo) => (
-              <>
-                <TodoCard key={todo.id} />
-              </>
-            ))}
-          </Suspense>
-        </SimpleGrid>
+        <TodosContainer todos={todos as Todo[]} />
       </main>
     </>
   );
